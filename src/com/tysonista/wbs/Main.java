1ce94bf4f34e3df6dc13bcb0a421b63f73a34c33
Taeyoung Son <tysonista@gmail.com>
2012-11-08 15:24:37 +0900

    [Title] Add osgi framwork. [Desc.] [Issue]

---
+++ src/com/tysonista/wbs/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tysonista.wbs;
+
+import org.tizen.common.core.command.*;
+
+/**
+ * @author tysonista <tysonista@gmail.com>
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Executor exe = new Executor();
+	}
+}
